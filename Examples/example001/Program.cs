bdf8b2f12575d4c28b891dd78f1f52f32a2ae83a
Violetta Davydova <write-here-pls@mail.ru>
2023-08-14 21:57:18 +0300

    test folder

---
+++ Examples/example001/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите число: ");
+string num = Console.ReadLine ()!;
+int number = int.Parse (num)!; // = int.Parse (Console.ReadLine ()!); - преобразовать в цифру то, что введут в console. Readline
+
+int square = number * number;
+Console.WriteLine ("Квадрат числа " + number + " равен " + square);
