b22766705e6c79e01dd75761f49ec37905f8ba85
Violetta Davydova <write-here-pls@mail.ru>
2023-08-19 13:34:39 +0300

    задача по семинару 2 про удалить среднее число

---
+++ Examples/example004_delete_middle_digit/Program.cs
@@ -0,0 +1,13 @@
+﻿// записать трехзначное число и удалить среднее, 567 -> 57
+
+int RemoveSecondDigit(int num) //функция
+{
+    int firstDigit = num / 100;
+    int thirdDigit = num % 10;
+    int result = firstDigit * 10 + thirdDigit;
+    return result;
+}
+int number = new Random().Next(100, 1000); //[100, 1000)
+int res = RemoveSecondDigit(number);
+
+Console.WriteLine($"в исходном числе {number} результат получим {res}");
