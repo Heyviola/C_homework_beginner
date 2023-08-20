1fcf721cd37d6eaf09caf513404ed8651657125a
Violetta Davydova <write-here-pls@mail.ru>
2023-08-20 08:57:44 +0300

    вывод ответа да или нет

---
+++ Seminar2_HW1/Program.cs
@@ -0,0 +1,17 @@
+﻿/* SЗадача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+int FindSecondDigit(int num) //функция
+{
+    int secondDigit = num / 10;
+    int firstDigit = num / 100;
+    int result = secondDigit - firstDigit * 10;
+    
+    return result;
+}
+int number = new Random().Next(100, 1000); //[100, 1000)
+int res = FindSecondDigit(number);
+
+Console.WriteLine($"в исходном числе {number} вторая цифра {res}");
