cc3d78044ca8c98ba813e3999e4122308848e698
Violetta Davydova <write-here-pls@mail.ru>
2023-08-20 07:13:26 +0300

    урок из лекции про void метод

---
+++ Examples/example005_deletel/Program.cs
@@ -0,0 +1,12 @@
+﻿// введите число, второе кратное первому или нет
+// можно через true и false
+int InputNum(string message) // функция ввода данных
+{
+    Console.WriteLine(message);
+    // int num = int.Parse(Console.ReadLine());
+    // return num;
+    return = int.Parse(Console.ReadLine());
+}
+
+int firstNum = InputNum ("Введите первое число ");
+int secondNum = InputNum("Введите второе число ");
