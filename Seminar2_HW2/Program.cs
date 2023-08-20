1906772392358d2264f63c3364c110862bb9a828
Violetta Davydova <write-here-pls@mail.ru>
2023-08-20 08:59:31 +0300

    new folder added

---
+++ Seminar2_HW2/Program.cs
@@ -0,0 +1,30 @@
+﻿/* Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, 
+что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6
+*/
+int FindThirdDigit(int number);
+{
+ if (number >=100)
+    {
+     while (number > 999)
+     {
+        number = number / 10;
+     }
+        int result = number % 10;
+    }
+    else
+    {
+     int result = -1;
+    }
+ return result;
+}
+
+Console.WriteLine("Введите число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+ 
+if (ThirdDigit(number1) == -1)
+Console.WriteLine("третьей цифры нет");
+else
+Console.WriteLine($"Third digit is {ThirdDigit(number1)}");
---
+++ Seminar2_HW3/Program.cs
@@ -0,0 +1,20 @@
+﻿/*Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+6 -> да
+7 -> да
+1 -> нет
+*/
+int dayOff(int number)
+{
+    int day = 0;
+    if(day >= 1 && day <= 5)
+    {
+    Console.WriteLine(" - нет");
+    }
+    Console.WriteLine(" - да");
+    return day;
+}
+Console.WriteLine("Введите номер дня недели: ");
+int number = int.Parse(Console.ReadLine()!);
+
+int  rest = dayOff(number);
+Console.WriteLine(rest);
